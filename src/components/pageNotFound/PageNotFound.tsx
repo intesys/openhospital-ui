3c9d978477d6f3354877c93eed1192545d63c37b
mpelosi <matteo.pelosi@intesys.it>
2019-10-22 12:12:44 +0200

    PATIENTNOTFOUND PAGE EDITED

--- src/components/pageNotFound/PageNotFound.tsx
+++ src/components/pageNotFound/PageNotFound.tsx
@@ -1,10 +1,32 @@
 import * as React from "react";
+import Typography from "@material-ui/core/Typography";
+
 
 class notFound extends React.Component {
+
     public render() {
         return (
-            <div>
-                pagina non trovata!
+            <div
+                style={{
+                        display: 'block',
+                        margin: 'auto',
+                        position: 'relative' }} >
+                <Typography color="inherit" style={{fontSize: 250,
+                    display: 'block',
+                    color: 'red',
+                    margin: 'auto',
+                    position: 'relative',
+                textAlign:'center'}}  >
+                    404
+                </Typography>
+                <Typography color="inherit" style={{fontSize: 100,
+                    display: 'block',
+                    margin: 'auto',
+                    position: 'relative',
+                    textAlign:'center'}}  >
+                    PAGE NOT FOUND
+                </Typography>
+
         </div>
         );
     }
